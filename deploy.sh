5475122af5785cb1232a817b375c323bf180f716
Arthur de Araújo Farias <adaraujo@niteurope.com>
2025-03-05 12:47:03 +0100

    Changed debug configuration and added systemd unit files.

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,16 @@
-scp build-target/clamir/clamir root@$1:/usr/bin
+#!/bin/bash -e
+
+echo "Reloading Stopping Clamir Linux Daemon"
+ssh -t root@$1 'systemctl stop clamir-linux-daemon'
+
+echo "Copying clamir-linux-daemon"
+scp build-target/clamir/clamir-linux-daemon root@$1:/usr/bin
+
+echo "Copying clamir-linux-daemon.service"
+scp share/clamir-linux-daemon.service root@$1:/lib/systemd/system
+
+echo "Reloading SystemD daemon"
+ssh -t root@$1 'systemctl daemon-reload'
+
+echo "Reloading Enabling SysteD Service"
+ssh -t root@$1 'systemctl enable clamir-linux-daemon'
--- launch-debug.sh
+++ launch-debug.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/bash -e
+
 ./build.sh
 ./deploy.sh 192.168.1.177
 ./debug.sh 192.168.1.177
