74f3539a7dff60f244e89b48013ebbb4df6420b3
Arthur de Araújo Farias <adaraujo@niteurope.com>
2025-03-05 14:05:40 +0100

    Trying to fix install directory of service file

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ echo "Copying clamir-linux-daemon"
 scp /home/arthur/Work/Software/clamir-linux-daemon/build-cmaketools/clamir-linux-daemon root@$1:/usr/local/bin
 
 echo "Copying clamir-linux-daemon.service"
-scp /root/clamir-linux-daemon/share/clamir-linux-daemon.service root@$1:/lib/systemd/system
+scp /home/arthur/Work/Software/clamir-linux-daemon/share/clamir-linux-daemon.service root@$1:/lib/systemd/system
 
 echo "Reloading SystemD daemon"
 ssh -t root@$1 'systemctl daemon-reload'
