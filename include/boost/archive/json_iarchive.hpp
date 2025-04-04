b3286ba9a08a28bf8df5e1c2f5c20512c7e360e2
Arthur de Araújo Farias <adaraujo@niteurope.com>
2025-04-04 11:05:57 +0200

    Chages configuration files from json to xml

---
+++ include/boost/archive/json_iarchive.hpp
@@ -0,0 +1,144 @@
+#ifndef BOOST_ARCHIVE_JSON_IARCHIVE_H
+#define BOOST_ARCHIVE_JSON_IARCHIVE_H
+
+// C++ Standard Library
+#include <iterator>
+#include <istream>
+#include <sstream>
+
+// Boost
+#include <boost/archive/detail/register_archive.hpp>
+
+// Boost
+#include <boost/archive/detail/common_iarchive.hpp>
+#include <boost/archive/detail/register_archive.hpp>
+
+// Boost Archive JSON
+#include <boost/archive/json.impl.hpp>
+#include <boost/serialization/array.hpp>
+#include <boost/serialization/item_version_type.hpp>
+#include <boost/serialization/nvp.hpp>
+
+namespace boost
+{
+namespace archive
+{
+  template<typename json_impl>
+class json_iarchive : public detail::common_iarchive<json_iarchive>
+{
+public:
+  explicit json_iarchive(std::istream& is);
+
+  ~json_iarchive() = default;
+
+  inline void load_start(const char* tag) { json_.ctx_start(tag); }
+
+  inline void load_end(const char* tag) { json_.ctx_end(tag); }
+
+  template <typename T> void load_override(const boost::serialization::nvp<T>& kv)
+  {
+    try
+    {
+      json_.ctx_start(kv.name());
+      this->load(kv.value());
+      json_.ctx_end(kv.name());
+    }
+    catch (const std::runtime_error& err)
+    {
+      std::ostringstream oss;
+      oss << '[' << kv.name() << "] : " << err.what();
+      throw json_archive_exception{oss.str()};
+    }
+    // ctx_start --> std::logic_error intentionally not caught
+  }
+
+  template <typename T>
+  std::enable_if_t<fusion::result_of::has_key<meta_type_conversions, T>::type::value> load_override(T& value)
+  {
+    load(value);
+  }
+
+  template <typename T>
+  std::enable_if_t<!fusion::result_of::has_key<meta_type_conversions, T>::type::value> load_override(T& value)
+  {
+    detail::common_iarchive<json_iarchive>::load_override(value);
+  }
+
+  template <typename T> void load(T& value)
+  {
+    if constexpr (fusion::result_of::has_key<picojson_native_types, T>::type::value)
+    {
+      value = json_.active().get<T>();
+    }
+    else if constexpr (fusion::result_of::has_key<picojson_conversions, T>::type::value)
+    {
+      using load_type = typename fusion::result_of::value_at_key<picojson_conversions, T>::type;
+      value = static_cast<T>(json_.active().template get<load_type>());
+    }
+    else if constexpr (std::is_same<class_name_type, T>::value)
+    {
+      value = class_name_type{json_.active().template get<std::string>().c_str()};
+    }
+    else if constexpr (std::is_same<serialization::collection_size_type, T>::value)
+    {
+      value = static_cast<std::size_t>(json_.active().template get<picojson_real_number_type>());
+    }
+    else if constexpr (fusion::result_of::has_key<meta_type_conversions, T>::type::value)
+    {
+      /// ???
+    }
+    else if constexpr (detail::is_fixed_size_array<T>::value)
+    {
+      auto& read_value_array = json_.active().get<picojson::array>();
+      auto witr = std::begin(value);
+
+      for (auto& v : read_value_array)
+      {
+        json_.ctx_push(v);
+        this->load(*witr++);
+        json_.ctx_pop();
+      }
+    }
+    else if constexpr (detail::is_std_vector_bool<T>::value)
+    {
+      auto& read_value_array = json_.active().get<picojson::array>();
+      value.reserve(read_value_array.size());
+
+      for (auto& v : read_value_array)
+      {
+        json_.ctx_push(v);
+        bool dst;
+        this->load(dst);
+        value.push_back(dst);
+        json_.ctx_pop();
+      }
+    }
+    else if constexpr (detail::is_std_vector<T>::value)
+    {
+      auto& read_value_array = json_.active().get<picojson::array>();
+      value.resize(read_value_array.size());
+
+      auto witr = value.begin();
+      for (auto& v : read_value_array)
+      {
+        json_.ctx_push(v);
+        this->load(*witr++);
+        json_.ctx_pop();
+      }
+    }
+    else
+    {
+      detail::common_iarchive<json_iarchive>::load_override(value);
+    }
+  }
+
+private:
+json_impl json_;
+};
+
+}  // archive
+}  // boost
+
+BOOST_SERIALIZATION_REGISTER_ARCHIVE(boost::archive::json_iarchive)
+
+#endif  // BOOST_ARCHIVE_JSON_IARCHIVE_H
---
+++ include/boost/archive/json_oarchive.hpp
@@ -0,0 +1,119 @@
+#ifndef BOOST_ARCHIVE_JSON_OARCHIVE_H
+#define BOOST_ARCHIVE_JSON_OARCHIVE_H
+
+// C++ Standard Library
+#include <iterator>
+#include <ostream>
+#include <sstream>
+#include <type_traits>
+
+// Boost
+#include <boost/archive/detail/common_oarchive.hpp>
+#include <boost/archive/detail/register_archive.hpp>
+
+// Boost Archive JSON
+#include <boost/archive/json_object.hpp>
+#include <boost/serialization/array.hpp>
+#include <boost/serialization/item_version_type.hpp>
+#include <boost/serialization/nvp.hpp>
+
+namespace boost
+{
+namespace archive
+{
+
+  template<typename json_impl>
+class json_oarchive : public detail::common_oarchive<json_oarchive>
+{
+public:
+  explicit json_oarchive(std::ostream& os, const bool prettify = false);
+
+  ~json_oarchive();
+
+  inline void save_start(const char* tag) { json_.ctx_start(tag); }
+
+  inline void save_end(const char* tag) { json_.ctx_end(tag); }
+
+  template <typename T> void save_override(const boost::serialization::nvp<T>& kv)
+  {
+    try
+    {
+      json_.ctx_start(kv.name());
+      json_.object_start();
+      this->save(kv.const_value());
+      json_.object_end();
+      json_.ctx_end(kv.name());
+    }
+    catch (const std::runtime_error& err)
+    {
+      std::ostringstream oss;
+      oss << '[' << kv.name() << "] : " << err.what();
+      throw json_archive_exception{oss.str()};
+    }
+    // ctx_start --> std::logic_error intentionally not caught
+  }
+
+  template <typename T>
+  std::enable_if_t<fusion::result_of::has_key<meta_type_conversions, T>::type::value> save_override(const T& value)
+  {
+    save(value);
+  }
+
+  template <typename T>
+  std::enable_if_t<!fusion::result_of::has_key<meta_type_conversions, T>::type::value> save_override(T& value)
+  {
+    detail::common_oarchive<json_oarchive>::save_override(value);
+  }
+
+  template <typename T> void save(const T& value)
+  {
+    if constexpr (fusion::result_of::has_key<picojson_native_types, T>::type::value)
+    {
+      json_.active() = picojson::value{value};
+    }
+    else if constexpr (fusion::result_of::has_key<picojson_conversions, T>::type::value)
+    {
+      using cast_type = typename fusion::result_of::value_at_key<picojson_conversions, T>::type;
+      json_.active() = picojson::value{static_cast<cast_type>(value)};
+    }
+    else if constexpr (fusion::result_of::has_key<meta_type_conversions, T>::type::value)
+    {
+      using cast_type = typename fusion::result_of::value_at_key<meta_type_conversions, T>::type;
+      auto cast_value = static_cast<cast_type>(value);
+      save_override(boost::serialization::make_nvp(fusion::at_key<T>(meta_type_names), cast_value));
+    }
+    else if constexpr (detail::is_std_vector<T>::value or detail::is_fixed_size_array<T>::value)
+    {
+      json_.array_start(std::distance(std::begin(value), std::end(value)));
+
+      for (const auto& element : value)
+      {
+        json_.array_push();
+        if constexpr (!detail::is_element_native_convertible<T>::value)
+        {
+          json_.object_start();
+        }
+        this->save(element);
+        json_.object_end();
+      }
+
+      json_.array_end();
+    }
+    else
+    {
+      detail::common_oarchive<json_oarchive>::save_override(value);
+    }
+  }
+
+private:
+json_impl json_;
+  std::ostream* os_;
+  bool prettify_;
+};
+
+}  // archive
+}  // boost
+
+BOOST_SERIALIZATION_REGISTER_ARCHIVE(boost::archive::json_oarchive)
+
+#endif  // BOOST_ARCHIVE_JSON_IARCHIVE_H
